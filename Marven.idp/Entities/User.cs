397a1ee998ccc14fa40844d28bb569347dce2133
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-28 10:45:43 +0530

    Added User and User Claims Data

---
+++ Marven.idp/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marven.idp.Entities
+{
+    public class User
+    {
+       
+        public Guid Id { get; set; }
+
+       
+        public string Subject { get; set; }
+
+       
+        public string UserName { get; set; }
+
+       
+        public string Password { get; set; }
+
+        public bool Active { get; set; }
+
+        public string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+    }
+}
---
+++ Marven.idp/Entities/UserClaims.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marven.idp.Entities
+{
+    public class UserClaims
+    {
+        public Guid Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Value { get; set; }
+
+        public string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+
+    }
+}
